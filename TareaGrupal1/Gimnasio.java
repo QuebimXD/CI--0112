01160c8e8e9708b46f2b8d4d6bf9d90064896547
xNicoleS5x <nikism05@gmail.com>
2025-09-28 17:29:32 -0600

    mas cambios en el gimnasio

--- TareaGrupal1/Gimnasio.java
+++ TareaGrupal1/Gimnasio.java
@@ -1,8 +1,10 @@
 import java.util.Random;
 public class Gimnasio {
     private String NombreEU;
+    private String Ataque;
     public Gimnasio(){
         NombreEU = "";
+        
     }
     public void setNombreEntrenador(String nombreU){
         NombreEU = nombreU;
@@ -11,12 +13,13 @@ public class Gimnasio {
         return NombreEU;
     }
     public void elegirAtaque(){
+        System.out.println("Elija un ataque:");
 
     }
     public void ataqueNPC(){
         Random rand = new Random();
         int numA = rand.nextInt(4); // numero entre 0 y el 3
-        
+
     }
     public void mostrarEntrenadores(){
 
@@ -24,4 +27,9 @@ public class Gimnasio {
     public void combate(){
 
     }
+    public int calcularDaño(int potencia, int presicion, int ataque, int defensa, int nivel){
+        int daño = (((2 * nivel / 5 + 2)* potencia * presicion/ defensa)/ 50) + 2;
+        return daño;
+    }
+
 }
