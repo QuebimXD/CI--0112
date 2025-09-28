7056294b7c46222e107ee7573eb2c3d3a2b76e82
lizsalazar17 <lizsalazara7@gmail.com>
2025-09-28 15:37:18 -0600

    Cambios en Elemento

--- TareaGrupal1/Elemento.java
+++ TareaGrupal1/Elemento.java
@@ -1,8 +1,33 @@
 public class Elemento {
     private String nombre;
-    private String[] debilidades;
-    private String[] fortalezas;
-    public Elemento(){
+    private String debilidad;
+    private String fortaleza;
 
+    public Elemento(String nombre, String debilidad, String fortaleza ){
+            this.nombre = nombre;
+            this.debilidad = debilidad;
+            this.fortaleza = fortaleza;
+            
+
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setDebilidades(String debilidad){
+        this.debilidad = debilidad;
+    }
+    public void setFortaleza(String fortaleza){
+        this.fortaleza = fortaleza;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getDebilidad(){
+        return debilidad;
+    }
+    public String getFortaleza(){
+        return fortaleza;
     }
 }
