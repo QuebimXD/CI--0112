c42fd4407991f164e8cf7bba64d2a97bff46cb73
lizsalazar17 <lizsalazara7@gmail.com>
2025-09-28 16:59:27 -0600

    Cambios Menu y creación de Entrenador

---
+++ TareaGrupal1/Entrenador.java
@@ -0,0 +1,20 @@
+public class Entrenador {
+    private String nombre;
+    private Pokemon[] equipoPokemon;
+
+    
+    public Entrenador(String nombre){
+        this.nombre = nombre;   
+    }
+
+    public boolean equipoDebilitado(){
+        return false;
+    }
+    public void agregarAlEquipoPokemon(Pokemon pokemon){
+
+    }
+    public void mostrarInfoEquipoPokemon(){
+
+    }
+
+}
--- TareaGrupal1/Menu.java
+++ TareaGrupal1/Menu.java
@@ -1,32 +1,69 @@
 import java.util.Scanner;
 public class Menu{
+    private Entrenador entrenador;
+    private Gimnasio gimnasio;
+
     public Menu(){
-        Gimnasio gimnasio = new Gimnasio();
-        int opcion = 0;
+        gimnasio = new Gimnasio();
         String nombre = "";
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¡Bienvenido al gimmnasio pokemon!");
+        System.out.println("Por favor, escriba su nombre:");
+        nombre = scanner.nextLine();
+        gimnasio.setNombreEntrenador(nombre);
+        System.out.println("Escoja a sus tres pokemon iniciales:");
+        seleccionJugador();
+
+        int opcion = 0;
         while (opcion <= 0){
-            System.out.println("¡Bienvenido al gimmnasio pokemon!");
-            System.out.println("Por favor, escriba su nombre:");
-            nombre = scanner.nextLine();
-            gimnasio.setNombreEntrenador(nombre);
-            System.out.println("Escoja a sus tres pokemon iniciales:");
-            seleccionJugador();
-            System.out.println("Empecemos el juego.");
-            System.out.println("Esto son los entrenadores con los cuales vas a competir: ");
-            gimnasio.mostrarEntrenadores();
-            System.out.println("Empieza tu primera batalla. ");
-            int entrenadoresDerrotados = 0;
-            gimnasio.combate(getEntrenador(0));
+
+            System.out.println("Empecemos el juego. Escoja una de las siguientes opciones:\n " +
+                              "1-Entrar al gimnasio.\n 2-Ver a los entrenadores\n "+
+                              "3-Ver mis pokemones y sus estadísticas.\n " +
+                              "4-Salir");
+            try {
+                opcion = scanner.nextInt();
+                if(opcion == 1){
+                    System.out.println("Empieza tu primera batalla.");
+                    int entrenadoresDerrotados = 0;
+                    while (entrenadoresDerrotados < 4){
+                        gimnasio.combate(gimnasio.getEntrenador(entrenadoresDerrotados));
+                        if(!entrenador.equipoDebilitado()){
+                        entrenadoresDerrotados++;
+                        }
+                        System.out.println("¡Has logrado terminar el gimmnasio!");
+                    }
+                }
+                else if (opcion == 2){
+                    System.out.println("Esto son los entrenadores con los cuales vas a competir: ");
+                    gimnasio.mostrarEntrenadores();
+                }
+                else if (opcion == 3){
+                    entrenador.mostrarInfoEquipoPokemon();
+                }
+                else if (opcion == 4){
+                    break;
+                }
             }
+            catch (Exception e) {
+                System.out.println("Escoja un número válido: ");
+
+            }
+        }
+
+
+            
     }
+
     public void seleccionJugador(){
         Scanner scanner = new Scanner(System.in);
         int opcion1 = 0;
         int opcion2 = 0;
         int opcion3 = 0;
-        Entrenador entrenador = new Entrenador();
-        entrenador.verCatalogo();
+        String nombre =gimnasio.getNombreEntrenador();
+        entrenador = new Entrenador(nombre);
+
         try {
             while(opcion1<=0 || opcion2 <= 0 || opcion3 <= 0){
                 System.out.println("Escoja tres pokemon de la siguiente lista.");
