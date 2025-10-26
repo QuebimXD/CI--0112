b97c93fc67773b3128192bee9acec91cc3ae7387
lizsalazar17 <lizsalazara7@gmail.com>
2025-10-25 18:50:26 -0600

    Constructor de clase y métodos get y set de Bloque

--- TareaGrupal2/Bloque.java
+++ TareaGrupal2/Bloque.java
@@ -1,3 +1,15 @@
 public class Bloque{
     private String color;
+
+    public Bloque(String color){
+        this.color = color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
 }
